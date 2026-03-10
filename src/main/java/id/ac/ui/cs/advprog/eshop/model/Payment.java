5068bf3cc8c9af14f1e44f7f7b6f2041e3aac10e
roseliaevanny <roseliasucipto8@gmail.com>
2026-03-10 11:35:05 +0700

    [RED] add Payment model skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -0,0 +1,26 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+public class Payment {
+    String id;
+    String method;
+    String status;
+    Map<String, String> paymentData;
+
+    public Payment(String id, String method, Map<String, String> paymentData) {
+    }
+
+    public Payment(String id, String method, String status, Map<String, String> paymentData) {
+    }
+
+    public void setStatus(String status) {
+    }
+}
