c749f99ffdd23150b43276a3d5d289455d8afbb7
roseliaevanny <roseliasucipto8@gmail.com>
2026-03-10 17:04:11 +0700

    [GREEN] implement PaymentServiceImpl

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -21,21 +21,45 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
-        return null;
+        String paymentId = UUID.randomUUID().toString();
+        Payment payment = new Payment(paymentId, method, PaymentStatus.PENDING.getValue(), paymentData);
+
+        if ("VOUCHER".equals(method)) {
+            String voucherCode = paymentData.get("voucherCode");
+            if (voucherCode != null
+                    && voucherCode.length() == 16
+                    && voucherCode.startsWith("ESHOP")
+                    && voucherCode.replaceAll("\\D", "").length() == 8) {
+                setStatus(payment, PaymentStatus.SUCCESS.getValue(), order);
+            } else {
+                setStatus(payment, PaymentStatus.REJECTED.getValue(), order);
+            }
+        }
+
+        return payment;
     }
 
     @Override
     public Payment setStatus(Payment payment, String status, Order order) {
-        return null;
+        if (!PaymentStatus.contains(status)) {
+            throw new IllegalArgumentException("Invalid payment status");
+        }
+        payment.setStatus(status);
+
+        if (PaymentStatus.SUCCESS.getValue().equals(status)) {
+            order.setStatus(OrderStatus.SUCCESS.getValue());
+        }
+
+        return paymentRepository.save(payment);
     }
 
     @Override
     public Payment getPayment(String paymentId) {
-        return null;
+        return paymentRepository.findById(paymentId);
     }
 
     @Override
     public List<Payment> getAllPayments() {
-        return null;
+        return paymentRepository.findAll();
     }
 }
